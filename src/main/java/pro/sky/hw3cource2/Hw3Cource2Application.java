50cd40cadfabad3a9cd92adc435bac7e4c05823a
Антон <antonL1790@yandex.ru>
2022-03-28 00:38:34 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hw3cource2/Hw3Cource2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw3cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3Cource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw3Cource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw3cource2/Hw3Cource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw3cource2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3Cource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
